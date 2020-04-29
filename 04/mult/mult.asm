a43d677afbbe7b5e33d4640fba93fab7b781266b
Pedro Bastos <pedro.bastos@unisoma.com>
2020-04-28 23:27:26 -0300

    Código assembly para multiplicação

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,47 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@count
+M=0 // contador para o loop
+
+@R2
+M=0 // zerando a saida
+
+(CHECK)
+
+    //checando se R0 é 0
+    D=0
+    @R0
+    D=D-M
+    @END
+    D; JEQ
+
+    //checando se R1 é 0
+    D=0
+    @R1
+    D=D-M
+    @END
+    D; JEQ
+
+
+(LOOP)
+    @R0
+    D=M //atribuindo R0 no registrador D
+
+    @R2
+    M=M+D // somando R0 em R2
+
+    //devo repetir o processo acima R1 vezes
+
+    @count
+    M=M+1 // incrementando a contagem
+    D=M // atribuindo contagem no registrador D
+
+    @R1
+    D=D-M // atribuindo R1 no registrador D
+
+    @LOOP
+    D; JNE
+
+(END)
