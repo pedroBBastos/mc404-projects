f82f63992d830c07fb1364a724a4d7e1d40275b9
Pedro Bastos <pedro.bastos@unisoma.com>
2020-05-02 23:39:31 -0300

    Fill.asm -> pintando tela de preto

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// criando simbolo 'fimDaTela' de controle para o fim da tela
+@24575 // 16384 (@SCREEN) + 8192 (256 linhas vezes 32 palavras por linha) - 1
+D=A
+@fimDaTela
+M=D
+
+@filled
+M=0
+
+
+(LOOP)
+
+    @KBD 
+    D=M // atribuindo o valor do que esta vindo do teclado ao registrador D
+
+    @LOOP
+    D; JEQ // voltar ao loop caso haja nada vindo do teclado
+    
+    //***********************
+    //  pensar em algo para limpar a tela
+    //***********************
+
+    // ver como criar constantes -> para o fim da tela
+
+    @SCREEN
+    D=A
+    @currentWord
+    M=D
+
+(BLACKENING)
+
+    //pegando endereço de memorias dos 16 pixels atuais
+    @currentWord
+    D=M
+    
+    //voltando A para apontar para os 16 pixels atuais para pinta-los
+    A=D
+    // 0000000000000000 para 1111111111111111 (pintando de preto todos os 16 bits)
+    M=!M
+
+    // atualizando @currentWord para depois pintar os proximos 16 pixels
+    @currentWord
+    M=D+1
+    
+    //operacoes para verificar se cheguei no final da tela
+    A=D // voltando A para apontar para a tela
+    D=A
+    @fimDaTela
+    D=M-D
+    @BLACKENING
+    D; JGT
+    
+    @LOOP
+    0; JMP
