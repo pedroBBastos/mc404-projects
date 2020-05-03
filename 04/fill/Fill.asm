b16dcfcebe089aef264044e8a91248bbac903ec9
Pedro Bastos <pedro.bastos@unisoma.com>
2020-05-03 11:02:16 -0300

    Dinâmica para pintar e limpar tela de acordo com o que vem do teclado

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -19,32 +19,41 @@ D=A
 @fimDaTela
 M=D
 
+//simbolo para controlar quando a tela esta preenchida ou nao
 @filled
 M=0
-
 
 (LOOP)
 
     @KBD 
     D=M // atribuindo o valor do que esta vindo do teclado ao registrador D
 
+    @CLEAR
+    D; JEQ 
+
+    //se ainda tiver tecla pressionada e já pintou apenas volto no loop
+    @filled
+    D=M
     @LOOP
-    D; JEQ // voltar ao loop caso haja nada vindo do teclado
-    
-    //***********************
-    //  pensar em algo para limpar a tela
-    //***********************
+    D-1; JEQ
+    @PAINT
+    D; JEQ
 
-    // ver como criar constantes -> para o fim da tela
+    //se tela esta preenchida, limpar. Se não, basta voltar ao loop
+(CLEAR)
+    @filled
+    D=M
+    @LOOP
+    D; JEQ
 
+(PAINT)
     @SCREEN
     D=A
     @currentWord
     M=D
+(PAINTING)
 
-(BLACKENING)
-
-    //pegando endereço de memorias dos 16 pixels atuais
+    //pegando endereço de memorias dos 16 pixels atuais e atribuindo ao registrador D
     @currentWord
     D=M
     
@@ -53,17 +62,29 @@ M=0
     // 0000000000000000 para 1111111111111111 (pintando de preto todos os 16 bits)
     M=!M
 
-    // atualizando @currentWord para depois pintar os proximos 16 pixels
+    // atualizando @currentWord para os proximos 16 pixels a serem pintados
     @currentWord
     M=D+1
     
     //operacoes para verificar se cheguei no final da tela
-    A=D // voltando A para apontar para a tela
-    D=A
     @fimDaTela
     D=M-D
-    @BLACKENING
+    @PAINTING
     D; JGT
-    
+
+    //antes de voltar a verificar o teclado, setar @filled
+    @filled
+    D=M
+    @RESETFILLED
+    D-1; JEQ
+    @filled
+    M=1
+    @LOOP
+    0; JMP
+
+(RESETFILLED)
+    @filled
+    M=0
+
     @LOOP
     0; JMP
